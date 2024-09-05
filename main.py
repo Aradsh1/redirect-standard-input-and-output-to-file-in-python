cc2f46c7103c7b8df864545c39d63e8faf4b5a57
= <=>
2024-09-05 10:49:23 +0330

    message

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+fin = open("./input.txt" , "rt")
+sys.stdin = fin
+
+while True:
+    numbers = input("->").strip().split(" ")
+    if numbers[0]=="add":
+        print(int(numbers[1]) + int(numbers[2]))
+    elif numbers[0]=="mul":
+        print(int(numbers[1]) * int(numbers[2]))
+    elif numbers[0]=="div":
+        print(int(numbers[1]) / int(numbers[2]))
+    elif numbers[0]=="min":
+        print(int(numbers[1]) - int(numbers[2]))
